940eea61efe87d64b5c40b3bf854dffec8f8460b
xhri <stempowski132@gmail.com>
2019-10-26 19:36:03 +0200

    Added clients app

---
+++ Client/MyActions/MyActions/FirstAction.cs
@@ -0,0 +1,14 @@
+﻿namespace MyActions
+{
+    using ExecutableNuget;
+    using System;
+
+    public class FirstAction : ICustomAction
+    {
+        public void Act(Configuration config)
+        {
+            Console.WriteLine("My first action started");
+            Console.WriteLine($"Name used in configuration: {config.Name}");
+        }
+    }
+}
